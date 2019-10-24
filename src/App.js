d1376e4966cf6f3572aa17e0f1aace403f33b5b9
Irene <irodri26@its.jnj.com>
2019-10-24 13:38:06 +0100

    Functional components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-class App extends React.Component {
-    render() {
-        return (
-            <h1>Hello World</h1>
-        );
-    }
-}
+// Class component
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <h1>Hello World</h1>
+//         );
+//     }
+// }
+
+// Functional component
+const App = () => (
+    <h1>Hello World</h1>
+);
 
 export default App;
